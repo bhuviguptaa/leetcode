59b7447f1018e4a3b59ff5c2585060ea845b3057
bhuviguptaa <bhuviguptaa12@gmail.com>
2026-09-26 14:46:02 +0530

    Time: 4 ms (13.68%), Space: 10.2 MB (28.56%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0;
-        int right = s.length()-1;
-        while(left<right){
-            while(left<right && !isalnum(s[left])) left++;
-            while(left<right && !isalnum(s[right])) right--;
-            if(tolower(s[left])!=tolower(s[right]))  return false;
-            left++; 
-            right--;
+        string ans;
+        for(int i=0;i<s.length();i++){
+            char ch= s[i];
+            if((ch >= 'A' && ch <= 'Z' ) || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= 
+            '9')){
+                ans.push_back(tolower(ch));
+            }
+        }
+        for(int i=0;i<ans.length()/2;i++){
+            if(ans[i] != ans[ans.length()-1-i]) return false;
         }
         return true;
     }
